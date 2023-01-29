a6a218740ded8c84c92aff15244bfc15e449409a
Priyanshi Somani <67621702+Priyanshii677@users.noreply.github.com>
2023-01-30 00:32:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const endDate = "23 March 2023 12:00 AM";
+const inputs = document.querySelectorAll("input");
+
+document.getElementById("end-date").innerHTML = endDate;
+
+const clock = () => {
+  const end = new Date(endDate);
+
+  const now = new Date();
+
+  const diff = (end - now) / 1000;
+  //convert days
+  const days = Math.floor(diff / 60 / 60 / 24);
+  inputs[0].value = days;
+  inputs[1].value = Math.floor((diff / 60 / 60) % 24);
+  inputs[2].value = Math.floor(((diff / 60) % 60) % 24);
+  inputs[3].value = Math.floor(((diff % 60) % 60) % 24);
+  console.log(inputs[1].value);
+  console.log(days);
+};
+
+clock();
